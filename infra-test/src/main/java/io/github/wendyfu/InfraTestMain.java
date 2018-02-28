092bc89230a02c67fe0d268dfaf1a7ed7ba59485
wendyfu <wendy@gdplabs.id>
2018-02-28 12:53:48 +0700

    Initial commit: - add working publish to artifactory with obstructed credential

---
+++ infra-test/src/main/java/io/github/wendyfu/InfraTestMain.java
@@ -0,0 +1,16 @@
+package io.github.wendyfu;
+
+/**
+ * InfraTestMain
+ * {Default file description, change this}
+ * Copyright (c) 2018 GDP Labs. All rights reserved.
+ *
+ * @author wendy
+ * @since Feb 28, 2018.
+ */
+public class InfraTestMain {
+
+    public static void printHelloWorld() {
+        System.out.println("Hello, World!");
+    }
+}
